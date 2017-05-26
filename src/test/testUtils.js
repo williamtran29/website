c83397e710d97356fd9ef30d48d96398cc8fc526
Greg Bergé <berge.greg@gmail.com>
2017-05-26 16:23:24 +0200

    tests: fix test env

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -4,6 +4,8 @@ import { shallow } from 'enzyme'
 import { ThemeProvider } from 'glamorous'
 import theme from 'client/theme'
 
+process.env.NODE_ENV = 'test'
+
 export const shallowWithTheme = (children, options) => {
   const wrapper = shallow(<ThemeProvider theme={theme}>{children}</ThemeProvider>, options)
   const instance = wrapper.root.instance()
