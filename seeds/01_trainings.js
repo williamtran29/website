f7890bd6e59023cbdd959eaaf4af166680b9d0c1
Greg Bergé <berge.greg@gmail.com>
2017-06-26 15:41:10 +0200

    feat: add node training

--- seeds/01_trainings.js
+++ seeds/01_trainings.js
@@ -84,6 +84,133 @@ Un projet sera réalisé en fil rouge. Vous pourrez ainsi mettre en pratique les
       slug: 'formation-javascript-es2017',
     },
     {
+      name: 'Node.js',
+      abstract: 'Libérez la puissance du JavaScript côté serveur.',
+      color: '#1F421E',
+      duration: 4,
+      price: 2000,
+      description: `En quelques années Node.js a complètement révolutionné le développement des applications web. Offrant des **performances optimales** et une **flexibilité sans égale**, Node.js a su s'imposer comme le choix de prédilection pour concevoir des applications web. **Basé sur le moteur V8 du navigateur Chrome**, il permet de **faire tourner le même code côté client et côté serveur**. Node.js tire également sa force de son **gestionnaire de paquet "npm"** qui contabilise plus de **475 000 modules disponibles**.
+
+Utilisé par tous [**les géants du web**](https://nodejs.org/en/foundation/case-studies/), Node.js est aujourd'hui incontournable. En plus de permettre la **création d'applications web réactives grâce à WebSocket**, Node.js permet d'utiliser les **nouveaux outils de développement en ligne de commande** comme Webpack, Babel ou ESLint.
+
+Dans cette formation vous découvrirez comment écrire du code **JavaScript moderne (ES2017) avec [Babel](https://babeljs.io/)**, profiter de l'écosystème grâce à **[npm](https://www.npmjs.com) et ses modules**,  maîtriser **l'asynchrone avec \`async\` / \`await\`**,  créer un **serveur web avec [Express](https://expressjs.com/) ou [Koa](http://koajs.com/)**,  créer des **interfaces réactives avec [Socket.io](https://socket.io/)** et écrire du **code isomorphique (client et serveur)**, **se connecter à une base de données avec [knex](http://knexjs.org/)**, **tester son code avec [Jest](https://facebook.github.io/jest/)**.
+
+Un projet complet sera réalisé en fil rouge durant toute la formation, cela vous permettra de mettre en pratique toutes les nouveautés qui vous seront présentées.
+
+### Objectifs
+- Savoir installer et utiliser Node.js et npm
+- Comprendre le fonctionnement de Node.js
+- Savoir créer une application web avec Node.js
+- Apprendre à structurer son code de manière optimale
+- Être capable de tester son code Node.js
+
+### Pré-requis
+- Des bases en JavaScript
+- Notions de fonctionnement basiques du web (client, server, HTTP,  requêtes, etc...)`,
+      outline: `### Jour 1 - Introduction à Node.js
+
+#### Introduction
+- Histoire de Node.js
+- Installer et mettre à jour Node.js avec nvm
+- Créer son premier programme Node.js
+
+#### ES2017
+- Présentation de l'évolution du langage JavaScript
+- Compatibilité avec Node.js
+- Utiliser le JavaScript de demain avec Babel
+- Destructuration et valeurs par défaut
+- Rest and spread
+- Template strings
+- \`let\` et \`const\`
+- Arrow functions
+- Modules ES2015
+
+#### Modulariser son code
+- Utilisation de la syntaxe ES2015 en Node.js
+- Différence avec la syntaxe \`require\`
+- Exporter et importer des modules
+- Découper son code de manière optimale
+
+#### Asynchrone
+- Historique de l'asynchrone
+- Les callbacks "node style"
+- Les promesses
+- \`async\` / \`await\`
+
+### Jour 2 - Les bases de Node.js
+
+#### Les modules de bases de node
+- \`console\`
+- \`process\` et \`os\`
+- \`global\`
+- \`url\` et \`util\`
+
+#### Evénements et flux
+- EventEmitter
+- Streams
+- Buffers
+- Interactions avec le système de fichiers
+
+#### Utilisation de npm & yarn
+- Installer des modules
+- Gérer et sécuriser les dépendances de son projet
+- Anatomie de \`package.json\`
+- Comment choisir ses modules ?
+- Publier un module npm
+
+#### Formatter et Linter son code
+- Formatter son code avec Prettier
+- Linter son code avec ESLint
+
+#### Tester et déboguer son application
+- La syntaxe BDD
+- Tester son code avec Jest
+- Connaître sa couverture de tests
+
+### Jour 3 - Création d'une application web avec Node.js
+
+#### Bootstap d'une application web
+- Structurer son code
+- Le module \`http\` de node
+- Présentation de Express et Koa
+- Servir des pages avec Koa
+- Relancer automatiquement son serveur avec nodemon
+- Templating avec EJS
+
+#### Interaction avec une base de donnée
+- Se connecter à une base donnée SQL avec Knex.js
+- Utiliser des modèles
+- Persister ses données
+
+#### Authentification et sessions
+- Authentification avec Passport.js
+- Sécuriser les sessions en Node.js
+
+### Jour 4 - Aller plus loin avec Node.js
+
+#### Test, debug et optimisation
+- Tester son code de bout en bout avec Selenium et Webdriver
+- Déboguer et optimiser son code avec l'inspecteur Chrome
+
+#### Websockets
+- Rendre son application reactive avec Socket.io
+- Découvrir comment scaler une application reactive
+
+#### Création d'outil en ligne de commande
+- Utiliser minimist ou commander
+- Déployer et packager un outil en ligne de commande
+
+#### Isomorphisme
+- Compiler son code pour le browser avec Webpack
+- Utiliser le même code côté client et serveur avec React
+
+#### Scaler une application Node.js
+- Utilisation du mode cluster de Node.js
+- Créer des jobs / workers`,
+      cloudinary_id: 'training-nodejs',
+      slug: 'formation-nodejs',
+    },
+    {
       name: 'React',
       abstract:
         'Apprenez à créer des interfaces web modernes, fluides et interactives',
