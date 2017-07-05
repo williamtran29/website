89572b38f7d0f8f35ae9a9119a7f3ba7648037af
Jeremy Sfez <jeremy@smooth-code.com>
2017-07-05 18:30:12 +0200

    feat(trainings): add printing format

---
+++ src/client/TrainingPdf.js
@@ -0,0 +1,289 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react'
+import styled from 'styled-components'
+import { Helmet } from 'react-helmet'
+import { gql, graphql } from 'react-apollo'
+import ReactMarkdown from 'react-markdown'
+import theme from 'style/theme'
+import { clUrl } from 'modules/cloudinary'
+import PageContainer from 'client/PageContainer'
+import MainTitle from 'modules/components/MainTitle'
+import Paragraph from 'modules/components/Paragraph'
+import Logo from 'client/Logo'
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  align-self: center;
+  max-width: 1000px;
+  width: 700px;
+  flex-direction: column;
+
+  @media (min-width: ${theme.medias.phablet}) {
+    flex-direction: row;
+  }
+`
+
+const Content = styled.div`
+  flex: 1;
+  margin: 20px 20px 20px;
+  font-size: 17px;
+  font-weight: 300;
+  letter-spacing: 0.2px;
+  line-height: 22px;
+  padding-bottom:40px;
+
+  strong {
+    font-weight: 400;
+  }
+
+  h3 {
+    font-size: 22px;
+    font-weight: 400;
+    letter-spacing: -0.2px;
+    line-height: 28px;
+  }
+}
+`
+
+const FloatRigthContainer = styled.div`float: right;`
+
+const ColoredLogo = styled(Logo)`
+  color: ${theme.colors.primary};
+  width: 160px;
+`
+
+const ParagraphPrint = styled(Paragraph)`
+  font-size:12px;
+  margin:0px;
+`
+
+const MainTitleSmall = styled(MainTitle)`
+  font-size: 20px;
+`
+
+const AbstractContent = styled.div`
+  height: 50px;
+  ${FloatRigthContainer} {
+    padding-left: 10px;
+    border-left: 1px solid black;
+  }
+`
+
+const Picture = styled.img`
+  width: 50px;
+  float: left;
+`
+
+const AbstractParagraph = styled(ParagraphPrint)`
+  height: 50px;
+  width: 380px;
+  padding-left:60px;
+  margin-top:40px;
+`
+
+const Section = styled.div`
+  h3 {
+    font-size: 14px;
+    margin: 15px 0 0 0;
+    padding: 0;
+    color: ${theme.colors.primary};
+    font-weight: bold;
+    line-height: 1.5em;
+  }
+
+  p {
+    padding: 0;
+    font-size: 12px;
+    line-height: 1.5em;
+  }
+
+  h3 + p {
+    margin: 0;
+  }
+`
+
+const Objectives = ({ description }) => {
+  const descriptionSplitting = description
+    .replace(/- /g, '')
+    .split('### Objectifs')
+  return (
+    <Section>
+      <ReactMarkdown source={`### Objectifs${descriptionSplitting[1]}`} />
+    </Section>
+  )
+}
+
+const SectionTitle = styled.h2`
+  margin: 20px 0 0 0;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${theme.colors.primary};
+`
+
+const Planning = styled.div`
+  flex: 1;
+  display: flex;
+  margin: 5px 0 10px 0;
+`
+
+const ProgrammeDay = styled.div`
+  margin-right: 5px;
+  flex: 1;
+  font-size: 11px;
+
+  h4 {
+    margin: 5px 0 0 0;
+    padding: 0;
+  }
+
+  ul {
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-left: 15px;
+  }
+
+  li {
+    margin: 0;
+    line-height: 1.2em;
+  }
+`
+const ProgrammeDayTitle = styled(SectionTitle)`
+  margin: 0 0 5px 0;
+  padding: 0;
+  ${'' /* margin-bottom:5px; */}
+  font-weight: bold;
+  border-left: 4px ${theme.colors.primary} solid;
+  padding-left: 10px;
+  font-size: 12px;
+  color: black;
+  line-height: 1em;
+`
+
+const ProgrammeArray = ({ programmeContent }) => {
+  const daySplitting = programmeContent.split(/### Jour \d+/)
+  daySplitting.shift()
+  const res = daySplitting.map((day, i) =>
+    <Planning key={i}>
+      <ProgrammeDay>
+        <ProgrammeDayTitle>
+          Jour {i + 1}
+        </ProgrammeDayTitle>
+        <ReactMarkdown source={day} />
+      </ProgrammeDay>
+    </Planning>,
+  )
+
+  return (
+    <Planning>
+      {res}
+      {/* {<ReactMarkdown source={daySplitting[1]} />} */}
+    </Planning>
+  )
+}
+
+const Description = ({ descriptionContent }) => {
+  const descriptionSplitting = descriptionContent.split('### Objectifs')
+
+  return (
+    <Section>
+      <ReactMarkdown source={`### Description\n${descriptionSplitting[0]}`} />
+    </Section>
+  )
+}
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 700px;
+  padding-top: 20;
+
+  hr {
+    width: 250px;
+  }
+`
+
+const TRAINING_QUERY = gql`
+  query trainingData($slug: ID!) {
+    training(slug: $slug) {
+      cloudinary_id
+      name
+      abstract
+      duration
+      slug
+      color
+      outline
+      description
+      price
+    }
+  }
+`
+
+const options = ({ match }) => ({ variables: { slug: match.params.slug } })
+
+const withTraining = graphql(TRAINING_QUERY, {
+  name: 'trainingData',
+  options,
+})
+
+export default withTraining(
+  ({ trainingData: { training } }) =>
+    training
+      ? <PageContainer>
+          <Helmet>
+            <title>
+              {`Formation ${training.name}`}
+            </title>
+          </Helmet>
+          <Container>
+            <Content>
+              <FloatRigthContainer>
+                <ColoredLogo />
+                <ParagraphPrint>www.smooth-code.com</ParagraphPrint>
+              </FloatRigthContainer>
+              <MainTitleSmall itemProp="name">
+                {`Formation ${training.name}`}
+              </MainTitleSmall>
+              <AbstractContent>
+                <FloatRigthContainer>
+                  <ParagraphPrint>
+                    <strong>DURÉE: </strong>
+                    {`${training.duration} ${training.duration > 1
+                      ? 'jours'
+                      : 'jours'}`}
+                    <br />
+                    <strong>PRIX: </strong>
+                    {`${training.price}`}
+                  </ParagraphPrint>
+                </FloatRigthContainer>
+                <Picture
+                  alt="icone"
+                  src={
+                    training &&
+                    clUrl(training.cloudinary_id, 'c_scale,w_50,h_50,dpr_2')
+                  }
+                />
+                <AbstractParagraph itemProp="description">
+                  {training.abstract}
+                </AbstractParagraph>
+              </AbstractContent>
+
+              <Objectives description={training.description} />
+              <SectionTitle>Programme</SectionTitle>
+              <ProgrammeArray programmeContent={training.outline} />
+              <Description descriptionContent={training.description} />
+              <Footer>
+                <hr />
+                <ParagraphPrint>
+                  <strong>Une question ?</strong> Vous avez besoin d’un
+                  renseignement ou d’une formation personnalisée ? Nous nous
+                  ferons un plaisir de répondre à vos questions.{' '}
+                  <strong>Tel.</strong> 06 50 58 80 79 - <strong>Mail :</strong>
+                  contact@smooth-code.com
+                </ParagraphPrint>
+              </Footer>
+            </Content>
+          </Container>
+        </PageContainer>
+      : null,
+)
