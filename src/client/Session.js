6a6662a2bc2c9175517fc0c8ec69e392e579934f
Greg Bergé <berge.greg@gmail.com>
2017-07-19 10:41:59 +0000

    feat: change og:description on session

--- src/client/Session.js
+++ src/client/Session.js
@@ -172,10 +172,7 @@ export default compose(
           <meta name="title" content={title} />
           <meta name="description" content={description} />
           <meta property="og:title" content={`Smooth Code - ${title}`} />
-          <meta
-            property="og:description"
-            content={training && training.abstract}
-          />
+          <meta property="og:description" content={description} />
           <meta property="og:type" content="website" />
           <meta property="og:image" content={training && training.ogImageUrl} />
           <meta name="twitter:card" content="summary_large_image" />
