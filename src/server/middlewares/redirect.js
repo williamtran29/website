6536c0aa801667290054db934d0cd4abb5751426
Greg Bergé <berge.greg@gmail.com>
2017-08-22 19:41:47 +0000

    fix: make 301 redirections

--- src/server/middlewares/redirect.js
+++ src/server/middlewares/redirect.js
@@ -2,6 +2,7 @@
 const redirect = matchers => async (ctx, next) => {
   for (const matcher of matchers) {
     if (ctx.request.url.match(matcher.match)) {
+      ctx.status = 301
       ctx.redirect(matcher.redirect)
       break
     }
