fa9a57f0b0323a49b1c4c890f0e586d5f7348768
Greg Bergé <berge.greg@gmail.com>
2017-10-28 16:49:13 +0000

    refactor: remove admin

--- src/client/admin/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from 'client/admin/App'
-
-const main = document.createElement('div')
-document.body.appendChild(main)
-
-render(<App />, main)
